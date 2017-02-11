0c39c1be540fb8c1f4315d35cd6f2b38e26b2686
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2017-02-12 00:02:31 +0100

    change jexcel to apache poi

--- app/src/main/java/fr/ralala/worktime/utils/ExcelHelper.java
+++ app/src/main/java/fr/ralala/worktime/utils/ExcelHelper.java
@@ -2,20 +2,24 @@ package fr.ralala.worktime.utils;
 
 import android.content.Context;
 
-import java.io.File;
-import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
-import jxl.CellView;
-import jxl.Workbook;
-import jxl.WorkbookSettings;
-import jxl.write.Formula;
-import jxl.write.Label;
-import jxl.write.Number;
-import jxl.write.WritableCellFormat;
-import jxl.write.WritableFont;
-import jxl.write.WritableSheet;
-import jxl.write.WritableWorkbook;
-import jxl.write.WriteException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Iterator;
+
 
 /**
  *******************************************************************************
@@ -27,79 +31,113 @@ import jxl.write.WriteException;
  *******************************************************************************
  */
 public class ExcelHelper {
-  private WritableCellFormat timesBold;
-  private WritableCellFormat times;
-  private WritableWorkbook workbook = null;
+  private CellStyle timesDateBold;
+  private CellStyle timesDate;
+  private CellStyle timesBold;
+  private CellStyle times;
+  private Workbook workbook = null;
+  private File file = null;
+  private CreationHelper createHelper = null;
 
-  public ExcelHelper(final Context ctx, final File file) throws IOException, WriteException{
-    WorkbookSettings wbSettings = new WorkbookSettings();
-    wbSettings.setLocale(ctx.getResources().getConfiguration().locale);
-    workbook = Workbook.createWorkbook(file, wbSettings);
-    initFonts();
+  public ExcelHelper(final Context ctx, final File file) {
+    this.file = file;
+    workbook = new HSSFWorkbook();  // or new XSSFWorkbook();
+    createHelper = workbook.getCreationHelper();
+    Font times10pt = workbook.createFont();
+    times10pt.setFontHeightInPoints((short)10);
+    times10pt.setFontName("Times");
+    Font times10ptBold = workbook.createFont();
+    times10ptBold.setFontHeightInPoints((short)10);
+    times10ptBold.setFontName("Times");
+    times10ptBold.setBold(true);
+    DataFormat df = workbook.createDataFormat();
+    times = workbook.createCellStyle();
+    times.setFont(times10pt);
+    timesBold = workbook.createCellStyle();
+    timesBold.setFont(times10ptBold);
+    timesDate = workbook.createCellStyle();
+    timesDate.setFont(times10pt);
+    timesDate.setDataFormat(df.getFormat("[h]:mm;@"));
+
+    timesDateBold = workbook.createCellStyle();
+    timesDateBold.setFont(times10ptBold);
+    timesDateBold.setDataFormat(df.getFormat("[h]:mm;@"));
   }
 
-  public WritableSheet createSheet(final String sheetTitle, int index) {
-    workbook.createSheet(sheetTitle, index);
-    return workbook.getSheet(index);
+  public Sheet createSheet(final String sheetTitle) {
+    workbook.createSheet(sheetTitle);
+    return workbook.getSheet(sheetTitle);
   }
 
-  public void write() throws IOException, WriteException {
-    workbook.write();
+  public void write() throws IOException {
+    FileOutputStream fileOut = new FileOutputStream(file);
+    workbook.write(fileOut);
+    fileOut.close();
     workbook.close();
   }
 
-  private void initFonts()  throws WriteException {
-    // Lets create a times font
-    WritableFont times10pt = new WritableFont(WritableFont.TIMES, 10);
-    // Define the cell format
-    times = new WritableCellFormat(times10pt);
-    // Lets automatically wrap the cells
-    times.setWrap(true);
-
-    // create create a bold font with unterlines
-    WritableFont times10ptBold = new WritableFont(
-      WritableFont.TIMES, 10, WritableFont.BOLD, false);
-    timesBold = new WritableCellFormat(times10ptBold);
-    // Lets automatically wrap the cells
-    timesBold.setWrap(true);
-    CellView cv = new CellView();
-    cv.setFormat(times);
-    cv.setFormat(timesBold);
-    cv.setAutosize(true);
-  }
-
-  public int createVerticalHeader(WritableSheet sheet, int row, int column, String[] headers)  throws WriteException {
-    for(String header : headers)
-      addLabel(sheet, row++, column, header, true);
-    return row;
-  }
-
-  public int createHorizontalHeader(WritableSheet sheet, int row, int column, String[] headers)  throws WriteException {
+  public int createHorizontalHeader(Sheet sheet, int row, int column, String[] headers) {
     for(String header : headers)
       addLabel(sheet, row, column++, header, true);
     return column;
   }
 
-  public void addFormula(WritableSheet sheet, int row, int column, StringBuilder formula) throws WriteException {
-    addFormula(sheet, row, column, formula.toString());
+  public void addFormula(Sheet sheet, int row, int column, StringBuilder formula, boolean bold, boolean time) {
+    addFormula(sheet, row, column, formula.toString(), bold, time);
   }
 
-  public void addFormula(WritableSheet sheet, int row, int column, String formula) throws WriteException {
-    Formula f = new Formula(column, row, formula);
-    sheet.addCell(f);
+  public void addFormula(Sheet sheet, int row, int column, String formula, boolean bold, boolean time) {
+    Cell cell = getCell(sheet, row, column);
+    if(!time)
+      cell.setCellStyle(bold ? timesBold : times);
+    else
+      cell.setCellStyle(bold ? timesDateBold : timesDate);
+    cell.setCellFormula(formula);
   }
 
-  public void addLabel(WritableSheet sheet, int row, int column, String s, boolean bold) throws WriteException {
-    Label label;
-    label = new Label(column, row, s, bold ? timesBold : times);
-    sheet.addCell(label);
+  public void addLabel(Sheet sheet, int row, int column, String s, boolean bold) {
+    Cell cell = getCell(sheet, row, column);
+    cell.setCellValue(createHelper.createRichTextString(s));
+    cell.setCellStyle(bold ? timesBold : times);
   }
 
-  public void addNumber(WritableSheet sheet, int row, int column,
-                         Integer integer, boolean bold) throws WriteException {
-    Number number;
-    number = new Number(column, row, integer, bold ? timesBold : times);
-    sheet.addCell(number);
+  public void addTime(Sheet sheet, int row, int column, String s, boolean bold) throws ParseException {
+    Cell cell = getCell(sheet, row, column);
+    cell.setCellFormula("TIME(" + s.replaceAll(":", ",") + ",00)"); // 00:00:00
+    cell.setCellStyle(bold ? timesDateBold : timesDate);
+  }
+
+  public void addNumber(Sheet sheet, int row, int column,
+                         double val, boolean bold) {
+    Cell cell = getCell(sheet, row, column);
+    cell.setCellValue(val);
+    cell.setCellStyle(bold ? timesBold : times);
+  }
+
+  private Cell getCell(Sheet sheet, int row, int column) {
+    Iterator<Row> itr = sheet.rowIterator();
+    Row r = null;
+    Cell c = null;
+    if (itr.hasNext()) {
+      while (itr.hasNext()) {
+        Row rr = itr.next();
+        if (rr.getRowNum() == row) {
+          r = rr;
+          Iterator<Cell> itc = r.cellIterator();
+          if (itc.hasNext()) {
+            while (itc.hasNext()) {
+              Cell cc = itc.next();
+              if (cc.getColumnIndex() == column) {
+                c = cc;
+                break;
+              }
+            }
+          }
+          break;
+        }
+      }
+    }
+    return c == null ? ((r == null ? sheet.createRow(row) : r).createCell(column)) : c;
   }
 
 }
