3150541c987e4dadb8c2ab4e24f987d7623d0afa
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2017-12-09 20:17:00 +0100

    Removes useless import

--- app/src/main/java/fr/ralala/worktime/ui/utils/UIHelper.java
+++ app/src/main/java/fr/ralala/worktime/ui/utils/UIHelper.java
@@ -22,7 +22,6 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
